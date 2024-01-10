55c7b37cbd955f4ee3eab3f1a24a7bfccb9e1ff6
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2024-01-10 19:03:10 +0530

    Project Created

---
+++ Neostore_SwiftUI/Neostore_SwiftUI/Neostore_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Neostore_SwiftUIApp.swift
+//  Neostore_SwiftUI
+//
+//  Created by Neosoft on 10/01/24.
+//
+
+import SwiftUI
+
+@main
+struct Neostore_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
