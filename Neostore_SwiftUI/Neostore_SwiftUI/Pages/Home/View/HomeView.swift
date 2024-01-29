76ede1e9e75f9ef32143f4b19929e9dc523a4f50
Aditya Ghadge <adityasghadge@gmail.com>
2024-01-29 20:22:37 +0530

    Home screen UI done

--- Neostore_SwiftUI/Neostore_SwiftUI/Pages/Home/View/HomeView.swift
+++ Neostore_SwiftUI/Neostore_SwiftUI/Pages/Home/View/HomeView.swift
@@ -11,8 +11,9 @@ struct HomeView: View {
     
     @State private var isSidebarOpened = false
     @State private var selection = 0
-    var sideBarWidth = UIScreen.main.bounds.size.width * 0.8
     var viewModel = HomeViewModel()
+    var screenWidth = UIScreen.main.bounds.width
+    var sideBarWidth = UIScreen.main.bounds.size.width * 0.8
     
     init() {
         UIPageControl.appearance().currentPageIndicatorTintColor = UIColor.red
@@ -40,9 +41,87 @@ struct HomeView: View {
                         .tabViewStyle(PageTabViewStyle())
                         .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
                         .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height*0.3)
+                        
+                        ScrollView(.vertical) {
+                            LazyVGrid(columns: [
+                                GridItem(.flexible(), spacing: 20),
+                                GridItem(.flexible(), spacing: 20)
+                            ], spacing: 20) {
+                                ForEach(0..<viewModel.furnitureData.count, id: \.self) { index in
+                                    if let data = viewModel.furnitureData[index] {
+                                        let title = data["name"] as? String ?? ""
+                                        let titlePosition = data["lblPosition"] as? Position
+                                        let imgName = data["imgName"] as? String ?? ""
+                                        let imgPosition = data["imgPosition"] as? Position ?? Position.topLeft
+                                        ZStack {
+                                            VStack {
+                                                switch titlePosition {
+                                                case .topLeft:
+                                                    Text(title)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                                        .padding([.top, .leading], 15)
+                                                        .font(.title3)
+                                                        .bold()
+                                                        .foregroundColor(.white)
+                                                case .topRight:
+                                                    Text(title)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                                                        .padding([.top, .trailing], 15)
+                                                        .font(.title3)
+                                                        .bold()
+                                                        .foregroundColor(.white)
+                                                case .bottomLeft:
+                                                    Text(title)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                                                        .padding([.bottom, .leading], 15)
+                                                        .font(.title3)
+                                                        .bold()
+                                                        .foregroundColor(.white)
+                                                case .bottomRight:
+                                                    Text(title)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                                                        .padding([.bottom, .trailing], 15)
+                                                        .font(.title3)
+                                                        .bold()
+                                                        .foregroundColor(.white)
+                                                default:
+                                                    Text("")
+                                                }
+                                            }
+                                            VStack {
+                                                switch imgPosition {
+                                                case .topLeft:
+                                                    Image(imgName)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
+                                                        .padding([.top, .leading], 15)
+                                                case .topRight:
+                                                    Image(imgName)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topTrailing)
+                                                        .padding(.top, -10)
+                                                        .padding(.trailing, 15)
+                                                case .bottomLeft:
+                                                    Image(imgName)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomLeading)
+                                                        .padding([.bottom, .leading], 15)
+                                                case .bottomRight:
+                                                    Image(imgName)
+                                                        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .bottomTrailing)
+                                                        .padding(.bottom, 10)
+                                                        .padding(.trailing, 0)
+                                                }
+                                            }
+                                        }
+                                        .frame(width: (screenWidth/2)-30, height: (screenWidth/2)-30)
+                                        .background(.red)
+                                    }
+                                }
+                            }
+                            .padding([.top, .bottom], 10)
+                            .padding([.leading, .trailing], 20)
+                        }
                     }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-                    .background(Color.red)
+                    .frame(width: .infinity, height: UIScreen.main.bounds.height*0.9)
+                    .background(Color.white)
                     .navigationBarTitleDisplayMode(.inline)
                     .navigationBarBackButtonHidden()
                     .toolbar {
