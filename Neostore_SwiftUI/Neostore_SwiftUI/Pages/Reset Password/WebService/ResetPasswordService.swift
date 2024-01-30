fae9e191ec646de9efa1c7bc76ea32e1d38db34f
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2024-01-30 17:03:37 +0530

    Reset password API Service

--- Neostore_SwiftUI/Neostore_SwiftUI/Pages/Reset Password/WebService/ResetPasswordService.swift
+++ Neostore_SwiftUI/Neostore_SwiftUI/Pages/Reset Password/WebService/ResetPasswordService.swift
@@ -11,7 +11,7 @@ import Combine
 
 class ResetPasswordService {
     
-    static func resetUserPassword(oldPassword:String, newPassword: String, confirmPassword: String) -> (AnyPublisher<(RegisterSuccessResponse?, RegisterDataAlreadyExist?, RegisterErrorResponse?), Error>) {
+    static func resetUserPassword(oldPassword:String, newPassword: String, confirmPassword: String) -> (AnyPublisher<(ResetPasswordResponse?,RegisterErrorResponse?), Error>) {
         
         let params = ["old_password": oldPassword, "password": newPassword, "confirm_password": confirmPassword] as [String: Any]
         
@@ -19,17 +19,13 @@ class ResetPasswordService {
         // operator which receives data, make changes and publishes that data
             .tryMap { data in
                 do {
-                    let responseData = try JSONDecoder().decode(RegisterSuccessResponse.self, from: data)
-                    if responseData.status == 422 {
-                        let responseData2 = try JSONDecoder().decode(RegisterDataAlreadyExist.self, from: data)
-                        return (nil, responseData2, nil)
-                    }
-                    return (responseData, nil, nil)
+                    let responseData = try JSONDecoder().decode(ResetPasswordResponse.self, from: data)
+                    return (responseData, nil)
                 }
                 catch {
                     do {
                         let responseData = try JSONDecoder().decode(RegisterErrorResponse.self, from: data)
-                        return (nil, nil, responseData)
+                        return (nil, responseData)
                     }
                     catch {
                         throw error
