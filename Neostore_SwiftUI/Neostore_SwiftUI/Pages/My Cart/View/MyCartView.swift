b96a607191945aab3498932902f72f138455bb4a
Aditya Ghadge <adityasghadge@gmail.com>
2024-02-06 11:33:26 +0530

    working code

--- Neostore_SwiftUI/Neostore_SwiftUI/Pages/My Cart/View/MyCartView.swift
+++ Neostore_SwiftUI/Neostore_SwiftUI/Pages/My Cart/View/MyCartView.swift
@@ -145,7 +145,7 @@ struct MyCartView: View {
                                                 .alert(isPresented: $viewModel.vmVars.showAlert) {
                                                     Alert(title: Text(AlertMessages.noteMsg.rawValue), message: Text(viewModel.vmVars.alertMessage))
                                                 }
-                                            NavigationLink(destination: AddAddressView()) {
+                                            NavigationLink(destination: AddressListView()) {
                                                 EmptyView()
                                             }
                                             .opacity(0)
