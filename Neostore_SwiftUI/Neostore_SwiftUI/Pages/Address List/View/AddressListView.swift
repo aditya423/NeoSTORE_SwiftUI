6feb50310b3bb68f4e6d66b63d18c92730699244
Aditya Ghadge <adityasghadge@gmail.com>
2024-02-06 15:42:21 +0530

    Address List Done

--- Neostore_SwiftUI/Neostore_SwiftUI/Pages/Address List/View/AddressListView.swift
+++ Neostore_SwiftUI/Neostore_SwiftUI/Pages/Address List/View/AddressListView.swift
@@ -10,7 +10,8 @@ import SwiftUI
 struct AddressListView: View {
     
     @StateObject var viewModel = AddressListViewModel()
-    @State var checked = true
+    @State private var checked = true
+    @State private var isButtonEnabled = true
     @Environment(\.presentationMode) var presentationMode: Binding<PresentationMode>
     
     var body: some View {
@@ -23,45 +24,76 @@ struct AddressListView: View {
                     if fulladdress.count != 0 {
                         if let address = fulladdress[0] {
                             HStack {
+                                Text("Shipping Address")
+                                Spacer()
+                            }
+                            .padding([.top, .horizontal], 20)
+                            
+                            HStack {
                                 Group {
                                     if checked {
                                         ZStack{
                                             Circle()
                                                 .fill(Color.gray)
-                                                .frame(width: 25, height: 25)
+                                                .frame(width: 20, height: 20)
                                             Circle()
                                                 .fill(AppColors.grayColor)
-                                                .frame(width: 15, height: 15)
+                                                .frame(width: 12, height: 12)
                                         }
                                         .onTapGesture {
                                             self.checked = false
+                                            self.isButtonEnabled = false
                                         }
                                     } else {
                                         ZStack{
                                             Circle()
                                                 .fill(Color.gray)
-                                                .frame(width: 25, height: 25)
+                                                .frame(width: 20, height: 20)
                                             Circle()
                                                 .fill(Color.white)
-                                                .frame(width: 15, height: 15)
+                                                .frame(width: 12, height: 12)
                                         }
                                         .onTapGesture {
                                             self.checked = true
+                                            self.isButtonEnabled = true
                                         }
                                     }
                                 }
+                                .padding(.leading, 20)
                                 
                                 Text(address)
                                     .padding(.horizontal, 10)
+                                
+                                Spacer()
                                 
                                 Image(ImageNames.cancel.rawValue)
                                     .onTapGesture {
                                         viewModel.removeAddress()
                                     }
+                                    .padding(.trailing, 20)
+                            }
+                            .padding(.top, 15)
+                            
+                            HStack {
+                                NavigationLink(destination: OrderListView()) {
+                                    Text("PLACE ORDER")
+                                        .foregroundColor(self.isButtonEnabled ? Color.white : AppColors.grayColor)
+                                        .font(.system(size: 25))
+                                        .bold()
+                                        .padding(10)
+                                        .frame(maxWidth: .infinity)
+                                        .background(.red)
+                                        .cornerRadius(5)
+                                }
+                                .disabled(isButtonEnabled ? false : true)
+                                .padding(.top, 15)
+                                .padding(.horizontal, 20)
                             }
                         }
                     }
                 }
+                
+                Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .background(.white)
@@ -85,7 +117,7 @@ struct AddressListView: View {
                         .foregroundColor(.white)
                 }
                 ToolbarItem(placement: .navigationBarTrailing) {
-                    NavigationLink(destination: AddAddressView()) {
+                    NavigationLink(destination: AddAddressView(addressListVM: viewModel)) {
                         Image(systemName: ImageNames.plusImage.rawValue)
                             .font(.title3)
                             .foregroundColor(.white)
