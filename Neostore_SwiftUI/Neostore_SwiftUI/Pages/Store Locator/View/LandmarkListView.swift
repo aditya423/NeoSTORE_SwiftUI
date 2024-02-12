c33984a1ad988e904dd6411b9d1e506aa2b9a9bb
Aditya Ghadge <adityasghadge@gmail.com>
2024-02-12 18:52:27 +0530

    Store Locator Done

--- Neostore_SwiftUI/Neostore_SwiftUI/Pages/Store Locator/View/LandmarkListView.swift
+++ Neostore_SwiftUI/Neostore_SwiftUI/Pages/Store Locator/View/LandmarkListView.swift
@@ -17,9 +17,18 @@ struct LandmarkListView: View {
             List(localSearchService.landmarks) { landmark in
                 VStack(alignment: .leading) {
                     Text(landmark.name)
+                        .padding(.horizontal, 20)
+                        .padding(.top, 15)
                     Text(landmark.title)
                         .opacity(0.5)
+                        .padding(.horizontal, 20)
+                        .padding(.bottom, 15)
+                    Spacer()
+                        .frame(maxWidth: .infinity, maxHeight: 1)
+                        .background(AppColors.grayColor)
                 }
+                .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                .listRowSeparator(.hidden)
                 .listRowBackground(localSearchService.landmark==landmark ? AppColors.grayColor : Color.white)
                 .onTapGesture {
                     localSearchService.landmark = landmark
